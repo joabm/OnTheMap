c83b91f7339967495dd2dab40fe371a3c477938f
joabm <84096469+joabm@users.noreply.github.com>
2022-09-04 19:58:14 -0700

    Struct for session response

--- OnTheMap/Model/Map Responses/SessionResponse.swift
+++ OnTheMap/Model/Map Responses/SessionResponse.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct SessionResponse: Codable {
+    let account: Account
+    let session: Session
+}
+
+struct Account: Codable {
+    let registered: Bool
+    let key: String
+}
+
+struct Session: Codable {
+    let id: String
+    let expiration: String
+}
