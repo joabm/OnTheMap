42bf1b07016df997c383d8b3ed96de72891c829c
joabm <84096469+joabm@users.noreply.github.com>
2022-09-04 19:24:10 -0700

    configured loginViewController

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import Foundation
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signupTextView: UITextView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        emailTextField.text = ""
+        passwordTextField.text = ""
+        
+        let signUpString = NSMutableAttributedString(string: "Don't have an account?  Sign Up")
+        let url = URL(string: "https://auth.udacity.com/sign-up?next=https://classroom.udacity.com")!
+        signUpString.setAttributes([.link: url], range: NSMakeRange(24, 7))
+        
+        signupTextView.attributedText = signUpString
+        
+        
     }
-
-
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    
 }
 
