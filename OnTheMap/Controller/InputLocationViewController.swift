fa33e2b4c8b5aa66c153622758789e1c8525d719
joabm <84096469+joabm@users.noreply.github.com>
2022-09-11 01:00:35 -0700

    adjusted keyboard controls to input location view

--- OnTheMap/Controller/InputLocationViewController.swift
+++ OnTheMap/Controller/InputLocationViewController.swift
@@ -8,21 +8,38 @@
 import Foundation
 import UIKit
 
-class InputLocationViewController: UIViewController {
+class InputLocationViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Outlets
     
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var urlTextField: UITextField!
     @IBOutlet weak var findLocationButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         locationTextField.text = ""
         urlTextField.text = ""
+        locationTextField.delegate = self
+        urlTextField.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
     
     
-    
+    // MARK: Actions
     
     @IBAction func findLocationTapped(_ sender: Any) {
         setIndicator(true)
@@ -36,6 +53,45 @@ class InputLocationViewController: UIViewController {
     @IBAction func cancelAddLocation(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    // MARK: Keyboard controls
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        //removes both observers at once
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    //keeps the keyboard from covering the input field by removing the height of the keyboard from the views frame.
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if urlTextField.isEditing && UIDevice.current.orientation.isLandscape || locationTextField.isEditing && UIDevice.current.orientation.isLandscape {
+            view.frame.origin.y = -125 //getKeyboardHeight(notification)
+        }
+    }
+    
+    //resets the frame height
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    //dismisses the keyboard when the return button is selected by the user
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    // MARK: setup and failure
     
     func setIndicator(_ isFinding: Bool) {
         if isFinding {
