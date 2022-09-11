99abe04598807ff31641cad7978e7ce00621d761
joabm <84096469+joabm@users.noreply.github.com>
2022-09-11 01:16:55 -0700

    added keyboard controls to Login View

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Foundation
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -16,14 +16,15 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    // MARK: Lifecycle
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         emailTextField.text = ""
         passwordTextField.text = ""
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
         
         let signUpString = NSMutableAttributedString(string: "Don't have an account?  Sign Up")
         let url = URL(string: "https://auth.udacity.com/sign-up?next=https://classroom.udacity.com")!
@@ -31,8 +32,19 @@ class LoginViewController: UIViewController {
         
         signupTextView.attributedText = signUpString
         
-        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    // MARK: Login Actions
     
     @IBAction func loginTapped(_ sender: Any) {
         setLoggingIn(true)
@@ -48,6 +60,45 @@ class LoginViewController: UIViewController {
             showLoginFailure(message: error?.localizedDescription ?? "")
         }
     }
+    
+    // MARK: Keyboard controls
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        //removes both observers at once
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    //keeps the keyboard from covering the input field by removing the height of the keyboard from the views frame.
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if emailTextField.isEditing && UIDevice.current.orientation.isLandscape || passwordTextField.isEditing && UIDevice.current.orientation.isLandscape {
+            view.frame.origin.y = -100 //getKeyboardHeight(notification)
+        }
+    }
+    
+    //resets the frame height
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    //dismisses the keyboard when the return button is selected by the user
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    // MARK: Setup and failure
     
     func setLoggingIn(_ loggingIn: Bool) {
         if loggingIn {
